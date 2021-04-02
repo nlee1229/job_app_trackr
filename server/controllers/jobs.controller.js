a8978ca254b34dc1b2a59e25b41dae0cea5cf56b
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-04-01 17:17:47 -0700

    commit

--- server/controllers/jobs.controller.js
+++ server/controllers/jobs.controller.js
@@ -1,7 +1,36 @@
-const { request } = require('express');
 const Job = require('../models/Job.model');
 
-// CRUD for jobs
-// get all of the properties
+// Method to find one job
+module.exports.getOneJob = (req, res) => {
+    Job.findOne(req.params._id) // find one job using id
+        .then(job => res.json(job))
+        .catch(err => res.json({ error: err }))
+}
 
-module.exports.getAllJobs = (req, res)
+// Method to find all jobs
+module.exports.getAllJobs = (req, res) => {
+    Job.find()
+        .then(allJobs => res.json({allJobs}))
+        .catch(err => res.json({ error: err }))
+}
+
+//  Method to create a new job
+module.exports.createJob = (req, res) => {
+    Job.create(req.body)
+        .then(newJob => res.json ({ newJob }))
+        // .catch(err => res.json({error: err}))
+        .catch(err => res.status(400).json(err));
+}
+
+// Method to delete a job
+module.exports.deleteJob = (req, res) => {
+    Job.deleteOne(req.params._id)
+        .then(confirmation => res.json(confirmation))
+        .catch(err => res.json(err))
+}
+
+module.exports.updateJob = (req, res) => {
+    Job.findByIdAndUpdate(req.params._id, req.body)
+        .then(updatedJob => res.json(updatedJob))
+        .catch(err => res.json({ error: err }))
+}
--- server/routes/jobs.routes.js
+++ server/routes/jobs.routes.js
@@ -1,9 +1,14 @@
-const routesctl = require('./controller');
+const routesCtl = require('..controllers/jobs.controller');
 
 module.exports = (app) => {
-  app.get('/api/users', routesctl.getAllUsers)
-  app.get('/api/users/:id', routesctl.getOneUser)
-  app.post('/api/users', routesctl.createUser)
-  app.put('/api/users/:id', routesctl.updateUser)
-  app.post('/api/users/:id/jobs', routesctl.addJobToUser)
+    // Route to find a single job
+    app.get('/api/jobs/:id', routesCtl.getOneJob);
+    // Route to find all jobs
+    app.get('/api/jobs', authenticate, routesCtl.getAllJobs);
+    // Route to create a job
+    app.post('/api/newJob', authenticate, routesCtl.createJob);
+    // Route to delete a job
+    app.delete('/api/delete/:id', routesCtl.deleteJob);
+    // Route to update a job
+    app.put('/api/update/:id', routesCtl.updateJob);
 }
